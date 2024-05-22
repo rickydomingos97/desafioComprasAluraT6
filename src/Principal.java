a6f9356665154a89f29f97f039cc9ae542a4d7bb
rickydomingos <ricardodomingos97@gmail.com>
2024-05-21 22:48:39 -0300

    update

--- src/Principal.java
+++ src/Principal.java
@@ -9,7 +9,7 @@ public class Principal {
         CartaoDeCredito cartao = new CartaoDeCredito(limite);
 
         int sair = 1;
-        while(sair != 0) {
+        while (sair != 0) {
             System.out.println("Digite a descrição da compra:");
             String descricao = leitura.next();
 
@@ -33,12 +33,11 @@ public class Principal {
         System.out.println("COMPRAS REALIZADAS:\n");
         Collections.sort(cartao.getCompras());
         for (Compra c : cartao.getCompras()) {
-            System.out.println(c.getDescricao() + " - " +c.getValor());
+            System.out.println(c.getDescricao() + " - " + c.getValor());
         }
         System.out.println("\n***********************");
 
-        System.out.println("\nSaldo do cartão: " +cartao.getSaldo());
+        System.out.println("\nSaldo do cartão: " + cartao.getSaldo());
     }
-
 
 }
